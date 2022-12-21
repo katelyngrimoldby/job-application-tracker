2bfe98cb1e3449395382c4a4ada192d43195ceab
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-21 15:40:27 -0700

    Build basic model

---
+++ backend/models/job.ts
@@ -0,0 +1,57 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../util/db';
+
+class Job extends Model {}
+Job.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    position_title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    company: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    applied: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    compensation: {
+      type: DataTypes.TEXT,
+      defaultValue: 'N/A',
+    },
+    status: {
+      type: DataTypes.TEXT,
+      defaultValue: 'applied',
+    },
+    interviews: {
+      type: DataTypes.ARRAY(DataTypes.DATE),
+    },
+    job_description: {
+      type: DataTypes.JSON,
+      allowNull: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'job',
+  }
+);
+
+export default Job;
