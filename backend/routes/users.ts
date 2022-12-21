e79f9b0438594725343b849d37f004e3bb25ea3a
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-21 16:51:25 -0700

    Add error handling

--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -1,25 +1,27 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
+import { User } from '../models';
 import userService from '../services/userService';
 
 const userRouter = express.Router();
 
-userRouter.get('/', (_req, res) => {
-  const result = userService.getAll();
+userRouter.get('/', async (_req, res) => {
+  const result = await userService.getAll();
 
   res.json(result);
 });
 
-userRouter.get('/:id', (req, res) => {
+userRouter.get('/:id', async (req, res) => {
   const id = req.params.id;
 
-  const result = userService.getOne(Number(id));
+  const result = await userService.getOne(Number(id));
   if (result) {
     return res.json(result);
   }
   return res.status(404).end();
 });
 
-userRouter.post('/', (req, res) => {
+userRouter.post('/', async (req, res) => {
   const { username, name, password } = req.body;
 
   if (!password) {
@@ -30,10 +32,15 @@ userRouter.post('/', (req, res) => {
       .status(400)
       .json({ error: 'password must be at least 3 characters long' });
   }
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+  try {
+    const user = await User.create({ username, name, passwordHash });
 
-  const result = userService.addNew(username, password, name);
-
-  res.json(result);
+    return res.json(user);
+  } catch (err) {
+    return res.status(400).send(err);
+  }
 });
 
 export default userRouter;
