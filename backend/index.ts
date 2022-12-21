348861ef042a791034eac52b206595c4c5a8a30f
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-21 16:30:13 -0700

    Implement middlewares and userRouter

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 import { PORT, POSTGRES_URL } from './util/config';
 import { connectToDatabase } from './util/db';
 
+import userRouter from './routes/users';
+
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (_req, res) => {
   res.send('working');
 });
+
+app.use('/api/users', userRouter);
 
 const start = async () => {
   console.log(POSTGRES_URL);
