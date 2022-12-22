59f7093df42e071c56e8a1cf982c2ca1593d7885
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-22 13:10:29 -0700

    Add model typing

--- backend/models/job.ts
+++ backend/models/job.ts
@@ -3,10 +3,22 @@ import {
   DataTypes,
   InferAttributes,
   InferCreationAttributes,
+  CreationOptional,
 } from 'sequelize';
 import { sequelize } from '../util/db';
 
-class Job extends Model<InferAttributes<Job>, InferCreationAttributes<Job>> {}
+class Job extends Model<InferAttributes<Job>, InferCreationAttributes<Job>> {
+  declare id: CreationOptional<number>;
+  declare positionTitle: string;
+  declare company: string;
+  declare location: string;
+  declare applied: string;
+  declare compensation: string;
+  declare status: 'applied' | 'interviewing' | 'offered' | 'rejected';
+  declare interviews: string[];
+  declare jobDescription: string;
+  declare userId: number;
+}
 Job.init(
   {
     id: {
@@ -14,7 +26,7 @@ Job.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    position_title: {
+    positionTitle: {
       type: DataTypes.TEXT,
       allowNull: false,
     },
@@ -41,7 +53,7 @@ Job.init(
     interviews: {
       type: DataTypes.ARRAY(DataTypes.DATE),
     },
-    job_description: {
+    jobDescription: {
       type: DataTypes.JSON,
       allowNull: true,
     },
