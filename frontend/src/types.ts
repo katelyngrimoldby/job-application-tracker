a0c887c0b4774b117a7ca64e4e582232110aaa28
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-22 10:32:12 -0700

    Update typeDef

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,10 +1,12 @@
 export interface Job {
   positionTitle: string;
-  companyName: string;
+  company: string;
   address: string;
   applied: string;
   compensation: string;
   status: 'applied' | 'interviewing' | 'offered' | 'rejected';
   interviews: string[];
   id: number;
+  jobDescription: string;
+  userId: number;
 }
