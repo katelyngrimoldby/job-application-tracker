570dfc0ebc30636c56ab5f9dc0253fbaad0366f1
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-22 16:23:16 -0700

    Add conditional rendering based on auth status

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,20 +1,48 @@
 import { Link } from 'react-router-dom';
+import { useStateValue, clearCurrentUser } from '../state';
+import { logout } from '../services/userAuth';
 
 const Menu = () => {
+  const [{ user }, dispatch] = useStateValue();
+
+  const handleClick = async () => {
+    if (user) {
+      await logout(user.token);
+      dispatch(clearCurrentUser());
+      window.localStorage.removeItem('User');
+    }
+  };
+
   return (
     <ul>
       <li>
-        <Link to='/'>Home</Link>
+        <Link to='/'>{user ? 'Your applications' : 'Home'}</Link>
       </li>
-      <li>
-        <Link to='/login'>Log In</Link>
-      </li>
-      <li>
-        <Link to='/register'>Register</Link>
-      </li>
-      <li>
-        <Link to='/new'>New Application</Link>
-      </li>
+      {!user && (
+        <>
+          <li>
+            <Link to='/login'>Log In</Link>
+          </li>
+          <li>
+            <Link to='/register'>Register</Link>
+          </li>
+        </>
+      )}
+      {user && (
+        <>
+          <li>
+            <Link to='/new'>New Application</Link>
+          </li>
+          <li>
+            <button
+              type='button'
+              onClick={handleClick}
+            >
+              Log out
+            </button>
+          </li>
+        </>
+      )}
     </ul>
   );
 };
