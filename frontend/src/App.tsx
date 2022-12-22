df93c210d6a1f70ed0c3cbbe83ec9ce41f280956
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-22 11:36:11 -0700

    Test w/ existing content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
 import { Footer } from './components/Layout';
-import RichTextEditor from './components/RichTextEditor';
+import ApplicationForm from './components/ApplicationForm';
+import { Job } from './types';
+
+const dummyJob: Job = {
+  jobDescription:
+    '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"teee"}]}]}',
+  positionTitle: 'test',
+  address: 'remote',
+  company: 'test inc',
+  applied: '2022-12-22',
+  compensation: '5k',
+  interviews: ['2022-12-22', '2021-01-02'],
+  status: 'offered',
+  id: 1,
+  userId: 1,
+};
 
 function App() {
   return (
     <>
       <p>Hello World!</p>
-      <RichTextEditor />
+      <ApplicationForm content={dummyJob} />
       <Footer />
     </>
   );
