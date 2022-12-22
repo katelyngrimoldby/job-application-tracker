54e986c1794ee34ed6c9fdc89b8002963dc453eb
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-22 15:53:24 -0700

    Add reducers for user management

--- frontend/src/state/reducer.ts
+++ frontend/src/state/reducer.ts
@@ -1,13 +1,30 @@
 import { State } from './state';
-import { Job } from '../types';
+import { Job, User } from '../types';
 
-export type Action = {
-  type: 'SET_JOB_LIST';
-  payload: Job[];
-};
+export type Action =
+  | {
+      type: 'SET_JOB_LIST';
+      payload: Job[];
+    }
+  | {
+      type: 'SET_CURRENT_USER';
+      payload: User;
+    }
+  | {
+      type: 'CLEAR_CURRENT_USER';
+      payload: null;
+    };
 
 export const setJobList = (payload: Job[]): Action => {
   return { type: 'SET_JOB_LIST', payload: payload };
+};
+
+export const setCurrentUser = (payload: User): Action => {
+  return { type: 'SET_CURRENT_USER', payload: payload };
+};
+
+export const clearCurrentUser = (): Action => {
+  return { type: 'CLEAR_CURRENT_USER', payload: null };
 };
 
 export const reducer = (state: State, action: Action): State => {
@@ -23,5 +40,17 @@ export const reducer = (state: State, action: Action): State => {
           ...state.jobs,
         },
       };
+    case 'SET_CURRENT_USER':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case 'CLEAR_CURRENT_USER':
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
   }
 };
