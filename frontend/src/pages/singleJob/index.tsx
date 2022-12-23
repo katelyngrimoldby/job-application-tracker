8d529b620c40a55a7f59762fb804726b781a2c73
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-23 14:12:55 -0700

    Add handling for deletion and link for updating

--- frontend/src/pages/singleJob/index.tsx
+++ frontend/src/pages/singleJob/index.tsx
@@ -1,14 +1,39 @@
+import { useState } from 'react';
+import { isAxiosError } from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 import { Job } from '../../types';
+import { deleteJob } from '../../services/jobs';
+import { useStateValue, removeJob } from '../../state';
 import Custom404 from '../custom404';
 import ReadOnlyRichText from '../../components/RichTextEditor/ReadOnly';
 
 const SingleJob = ({ job }: { job: Job | null | undefined }) => {
+  const navigate = useNavigate();
+  const [{ user }, dispatch] = useStateValue();
+  const [error, setError] = useState('');
+
+  if (!user) {
+    return <h2>401 Unauthorized</h2>;
+  }
+
   if (!job) {
     return <Custom404 />;
   }
+  const handleDelete = async () => {
+    try {
+      await deleteJob(user.token, job.id);
+      dispatch(removeJob(job.id));
+      navigate('/jobs');
+    } catch (err) {
+      if (isAxiosError(err)) {
+        setError(err.response?.data);
+      }
+    }
+  };
 
   return (
     <>
+      {error && <p>{error}</p>}
       <h2>{job.positionTitle}</h2>
       <h3>{job.company}</h3>
       <div>
@@ -32,6 +57,13 @@ const SingleJob = ({ job }: { job: Job | null | undefined }) => {
         <b>Job Description</b>
       </p>
       <ReadOnlyRichText content={job.jobDescription} />
+      <Link to={`/jobs/${job.id}/edit`}>Edit</Link>
+      <button
+        type='button'
+        onClick={handleDelete}
+      >
+        Delete
+      </button>
     </>
   );
 };
