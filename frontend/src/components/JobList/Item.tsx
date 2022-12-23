dccd3202adbc350b7b40a0ad58b0dc3f8f652e7a
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-23 13:01:12 -0700

    Add link to full application

--- frontend/src/components/JobList/Item.tsx
+++ frontend/src/components/JobList/Item.tsx
@@ -1,13 +1,14 @@
+import { Link } from 'react-router-dom';
 import { Job } from '../../types';
-import ReadOnlyRichText from '../RichTextEditor/ReadOnly';
 
 const JobListItem = ({ job }: { job: Job }) => {
-  const jobDescStringify = job.jobDescription.replaceAll("'", '"');
-
   return (
     <div>
-      <h2>{job.positionTitle}</h2> <span>{job.company}</span>
-      <ReadOnlyRichText content={jobDescStringify} />
+      <h2>{job.positionTitle}</h2>
+      <div>
+        <span>{job.company}</span> <span>{job.status}</span>
+      </div>
+      <Link to={`/jobs/${job.id}`}>View Application</Link>
     </div>
   );
 };
