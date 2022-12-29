d88892173e20bb05d9ca0dbd0442bf7bed5d5ff7
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-29 14:36:47 -0700

    Import global css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { reducer, StateProvider } from './state';
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
