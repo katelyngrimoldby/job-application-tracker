660fe8fe4a7b5de25971e28ca3e70e263556500d
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-30 11:48:03 -0700

    Add classNames

--- frontend/src/components/JobList/Item.tsx
+++ frontend/src/components/JobList/Item.tsx
@@ -1,15 +1,44 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Job } from '../../types';
+import toggleArrow from '../../assets/menu-down.svg';
+import styles from '../../styles/components/JobList/Item.module.css';
 
 const JobListItem = ({ job }: { job: Job }) => {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <div>
-      <h2>{job.positionTitle}</h2>
-      <div>
-        <span>{job.company}</span> <span>{job.status}</span>
+    <li>
+      <div className={styles.primaryInfo}>
+        <b>{job.positionTitle}</b> <span>{job.company}</span>
+        <button onClick={() => setVisible(!visible)}>
+          <img
+            src={toggleArrow}
+            alt={visible ? 'Collapse Application' : 'Expand Application'}
+            width='24'
+            height='24'
+            className={visible ? styles.collapseBtn : styles.expandBtn}
+          />
+        </button>
       </div>
-      <Link to={`/jobs/${job.id}`}>View Application</Link>
-    </div>
+      <div className={visible ? styles.extraVisible : styles.extraInfo}>
+        <div>
+          <span>{job.location}</span> <span>{job.compensation}</span>
+        </div>
+        <div>
+          <span>Applied on {job.applied.substring(0, 10)}</span>{' '}
+          <span>{job.status}</span>
+        </div>
+        <div>
+          <Link
+            to={`/jobs/${job.id}`}
+            className='primary'
+          >
+            View Application
+          </Link>
+        </div>
+      </div>
+    </li>
   );
 };
 
