777b50176cdded7803872f2f20ffe1219da334e8
katelyngrimoldby <katelyngrimoldby@gmail.com>
2023-01-02 16:01:48 -0700

    Style for desktop layout

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,8 +1,9 @@
 import LoginForm from '../components/LoginForm';
+import styles from '../styles/pages/login.module.css';
 
 const Login = () => {
   return (
-    <main>
+    <main className={styles.main}>
       <h2>Login</h2>
       <LoginForm />
     </main>
