0f87ce201eefa0d26a2f55a6412df6787248f448
katelyngrimoldby <katelyngrimoldby@gmail.com>
2023-01-03 10:58:16 -0700

    Write helper fns for query params

---
+++ backend/util/filtrationHelper.ts
@@ -0,0 +1,32 @@
+import { Order } from 'sequelize';
+import { Status } from '../types';
+
+const getOrder = (sort: string | undefined): Order => {
+  switch (sort) {
+    case 'company-asc':
+      return [['company', 'ASC']];
+    case 'company-desc':
+      return [['company', 'DESC']];
+    case 'position-asc':
+      return [['positionTitle', 'ASC']];
+    case 'position-desc':
+      return [['positionTitle', 'DESC']];
+    case 'newest':
+      return [['applied', 'DESC']];
+    case 'oldest':
+      return [['applied', 'ASC']];
+    default:
+      return [['applied', 'DESC']];
+  }
+};
+
+const getFilter = (
+  status: Status | undefined
+): { status?: Status } | Record<string, never> => {
+  if (status) {
+    return { status: status };
+  }
+
+  return {};
+};
+export { getOrder, getFilter };
