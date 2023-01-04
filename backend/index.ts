6ecff5a140f98563827c7ebc491ca0292e929d69
katelyngrimoldby <katelyngrimoldby@gmail.com>
2023-01-04 11:43:38 -0700

    Move auth routes and add session fetch

--- backend/index.ts
+++ backend/index.ts
@@ -6,9 +6,7 @@ import { PORT } from './util/config';
 import { connectToDatabase } from './util/db';
 import tokenExtractor from './middleware/tokenExtractor';
 import errorHandler from './middleware/errorHandler';
-import { RequestUserAuth } from './types';
-
-import authService from './services/authService';
+import authRouter from './routes/auth';
 import userRouter from './routes/users';
 import jobRouter from './routes/jobs';
 
@@ -19,22 +17,7 @@ app.get('/', (_req, res) => {
   res.send('working');
 });
 
-app.post('/api/login', async (req, res) => {
-  const { username, password } = req.body;
-  const result = await authService.login(username, password);
-
-  res.json(result);
-});
-
-app.delete('/api/logout', tokenExtractor, async (req: RequestUserAuth, res) => {
-  if (req.decodedToken) {
-    const result = await authService.logout(req.decodedToken.id);
-
-    return res.status(204).json(result);
-  }
-  return res.status(401).json({ error: 'Missing authentication' });
-});
-
+app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/jobs', tokenExtractor, jobRouter);
 
---
+++ backend/routes/auth.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import tokenExtractor from '../middleware/tokenExtractor';
+import { RequestUserAuth } from '../types';
+import authService from '../services/authService';
+
+const authRouter = Router();
+
+authRouter.get('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const session = await authService.getSession(Number(id));
+
+  if (!session) {
+    res.status(204).send(null);
+  }
+
+  res.json(session);
+});
+
+authRouter.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const result = await authService.login(username, password);
+
+  res.json(result);
+});
+
+authRouter.delete(
+  '/logout',
+  tokenExtractor,
+  async (req: RequestUserAuth, res) => {
+    if (req.decodedToken) {
+      const result = await authService.logout(req.decodedToken.id);
+
+      return res.status(204).json(result);
+    }
+    return res.status(401).json({ error: 'Missing authentication' });
+  }
+);
+
+export default authRouter;
