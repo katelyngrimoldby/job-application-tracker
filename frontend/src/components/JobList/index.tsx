43b0f45e7b809934cbda1161f2af757228fe2551
katelyngrimoldby <katelyngrimoldby@gmail.com>
2023-01-03 21:05:08 -0700

    Add cta for empty job list

--- frontend/src/components/JobList/index.tsx
+++ frontend/src/components/JobList/index.tsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
 import { Job } from '../../types';
 import JobListItem from './Item';
 import styles from '../../styles/components/JobList/JobList.module.css';
 
 const JobList = ({ jobs }: { jobs: Job[] }) => {
+  if (jobs.length <= 0) {
+    return (
+      <div className={styles.cta}>
+        <p>No job applications yet</p>
+        <Link
+          to='/new'
+          className='primary'
+        >
+          Track a New One
+        </Link>
+      </div>
+    );
+  }
   return (
     <ul className={styles.list}>
       {Object.values(jobs).map((job) => (
