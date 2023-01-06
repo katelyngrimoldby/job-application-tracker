73c811663a009c832d17e5710cdea724396ce2a3
katelyngrimoldby <katelyngrimoldby@gmail.com>
2023-01-06 16:45:24 -0700

    Extract app to importable module

---
+++ backend/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+import 'express-async-errors';
+import tokenExtractor from './middleware/tokenExtractor';
+import errorHandler from './middleware/errorHandler';
+import authRouter from './routes/auth';
+import userRouter from './routes/users';
+import jobRouter from './routes/jobs';
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('working');
+});
+
+app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
+app.use('/api/jobs', tokenExtractor, jobRouter);
+
+app.use(errorHandler);
+
+export default app;
--- backend/index.ts
+++ backend/index.ts
@@ -1,27 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-const app = express();
-import 'express-async-errors';
 import { PORT } from './util/config';
 import { connectToDatabase } from './util/db';
-import tokenExtractor from './middleware/tokenExtractor';
-import errorHandler from './middleware/errorHandler';
-import authRouter from './routes/auth';
-import userRouter from './routes/users';
-import jobRouter from './routes/jobs';
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (_req, res) => {
-  res.send('working');
-});
-
-app.use('/api/auth', authRouter);
-app.use('/api/users', userRouter);
-app.use('/api/jobs', tokenExtractor, jobRouter);
-
-app.use(errorHandler);
+import app from './app';
 
 const start = async () => {
   await connectToDatabase();
