66df4baf533c828f5e6c163391daeaf749ad6701
katelyngrimoldby <katelyngrimoldby@gmail.com>
2023-01-06 21:37:53 -0700

    Write first two test cases

---
+++ backend/__tests__/tokenExtractor.test.ts
@@ -0,0 +1,113 @@
+import { redis, sequelize, connectToDatabase } from '../util/db';
+import { User, Job } from '../models';
+import helper from '../util/testHelper';
+import { NextFunction, Response } from 'express';
+import { RequestUserAuth } from '../types';
+import userService from '../services/userService';
+// import authService from '../services/authService';
+import tokenExtractor from '../middleware/tokenExtractor';
+
+describe('Token extractor middleware', () => {
+  let mockRequest: Partial<RequestUserAuth>;
+  let mockResponse: Partial<Response>;
+  const mockNext: NextFunction = jest.fn();
+
+  beforeAll(async () => {
+    await connectToDatabase();
+  });
+
+  beforeEach(async () => {
+    await redis.FLUSHALL();
+    await Job.truncate({ cascade: true });
+    await User.truncate({ cascade: true });
+
+    await userService.addNew(helper.initialUsers[0]);
+
+    mockRequest = { headers: {} };
+    mockResponse = {
+      status: jest.fn((_code) => mockResponse as Response),
+      json: jest.fn(),
+    };
+  });
+
+  it('Returns missing token error if authorization header missing', () => {
+    tokenExtractor(
+      mockRequest as RequestUserAuth,
+      mockResponse as Response,
+      mockNext
+    );
+
+    expect(mockResponse.status).toBeCalledWith(401);
+    expect(mockResponse.json).toBeCalledWith({ error: 'Token missing' });
+  });
+
+  it('Returns invalid token error if verification fails', () => {
+    mockRequest = {
+      headers: {
+        authorization: 'bearer ewst56ty9ihiu89huj',
+      },
+    };
+
+    tokenExtractor(
+      mockRequest as RequestUserAuth,
+      mockResponse as Response,
+      mockNext
+    );
+
+    expect(mockNext).toBeCalledTimes(1);
+    expect(mockNext).toBeCalledWith(
+      expect.objectContaining({ name: 'JsonWebTokenError' })
+    );
+  });
+
+  // it('Saves token payload if valid token', async () => {
+  //   const user = helper.initialUsers[0];
+  //   const loginRes = await authService.login(user.username, user.password);
+
+  //   mockRequest = {
+  //     headers: {
+  //       authorization: `bearer ${loginRes.session.token}`,
+  //     },
+  //   };
+
+  //   tokenExtractor(
+  //     mockRequest as RequestUserAuth,
+  //     mockResponse as Response,
+  //     mockNext
+  //   );
+
+  //   expect(mockRequest.decodedToken).toEqual({
+  //     iat: expect.any(Number),
+  //     id: loginRes.id,
+  //     username: user.username,
+  //   });
+  // });
+
+  // it('Returns invalid session error if user has no session', async () => {
+  //   const user = helper.initialUsers[0];
+  //   const loginRes = await authService.login(user.username, user.password);
+
+  //   await authService.logout(loginRes.id);
+
+  //   mockRequest = {
+  //     headers: {
+  //       authorization: `bearer ${loginRes.session.token}`,
+  //     },
+  //   };
+
+  //   tokenExtractor(
+  //     mockRequest as RequestUserAuth,
+  //     mockResponse as Response,
+  //     mockNext
+  //   );
+
+  //   // expect(mockResponse.status).toBeCalledWith(401);
+  //   expect(mockNext).toBeCalledWith();
+  //   expect(mockResponse.json).toBeCalledWith({ error: 'Session invalid' });
+  // });
+
+  afterAll(async () => {
+    await sequelize.close();
+    await redis.disconnect();
+  });
+});
