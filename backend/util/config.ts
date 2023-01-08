d34a07e9fef0916047b30c092c28e92cf26e809a
katelyngrimoldby <katelyngrimoldby@gmail.com>
2023-01-08 16:16:14 -0700

    Update port for deployment

--- backend/util/config.ts
+++ backend/util/config.ts
@@ -5,6 +5,6 @@ export const POSTGRES_URL = process.env.POSTGRES_URL;
 
 export const REDIS_URL = process.env.REDIS_URL;
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 8080;
 
 export const SECRET = process.env.SECRET;
