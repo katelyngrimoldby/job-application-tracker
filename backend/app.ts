e6400fded645e75c851ad0b903d180b7bfd49276
katelyngrimoldby <katelyngrimoldby@gmail.com>
2023-01-08 16:20:20 -0700

    Add middleware and path matching for production

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 import express from 'express';
-import cors from 'cors';
 const app = express();
 import 'express-async-errors';
+import path from 'path';
 import tokenExtractor from './middleware/tokenExtractor';
 import sessionValidator from './middleware/sessionValidator';
 import errorHandler from './middleware/errorHandler';
@@ -9,16 +12,25 @@ import authRouter from './routes/auth';
 import userRouter from './routes/users';
 import jobRouter from './routes/jobs';
 
-app.use(cors());
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.send('working');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build/dist'));
+}
+
+app.get('/api/ping', (_req, res) => {
+  res.send('pong');
 });
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/jobs', tokenExtractor, sessionValidator, jobRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
+}
 
 app.use(errorHandler);
 
