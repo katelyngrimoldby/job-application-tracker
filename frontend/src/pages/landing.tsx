6777a5cdcf6427a61f91f44d5e1b3ab299927053
katelyngrimoldby <katelyngrimoldby@gmail.com>
2023-01-08 19:19:57 -0700

    Build basic landing page
    This page is incredibly barebones and must be properly designed and built

--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -1,7 +1,22 @@
+import { Link } from 'react-router-dom';
+import styles from '../styles/pages/landing.module.css';
+
 const Landing = () => {
   return (
-    <main>
-      <h2>Landing Page</h2>
+    <main className={styles.main}>
+      <header>
+        <h1 className={styles.header}>Job hunting?</h1>
+        <p className={styles.subtext}>
+          Utilize this job tracker to manage your applications&apos; statuses,
+          interviews, and even job postings - all in one easy-to-use platform.
+        </p>
+      </header>
+      <Link
+        to='/register'
+        className='primary'
+      >
+        Sign up - It&apos;s Free
+      </Link>
     </main>
   );
 };
