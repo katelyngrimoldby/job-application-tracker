48b61e44b0139d0f995798d524640c2714b92292
katelyngrimoldby <katelyngrimoldby@gmail.com>
2023-01-08 20:10:35 -0700

    Add links to legal docs

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -33,8 +33,26 @@ const Header = () => {
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>Privacy Policy</p>
-      <p>Designed and developed by Katelyn Grimoldby © 2022</p>
+      <p>
+        Designed and developed by{' '}
+        <a href='https://www.katelyngrimoldby.com/'>Katelyn Grimoldby</a> © 2022
+      </p>
+      <p>
+        <a
+          href='/terms-and-conditions.pdf'
+          target='_blank'
+        >
+          Terms and Conditions
+        </a>
+      </p>
+      <p>
+        <a
+          href='/privacy-policy.pdf'
+          target='_blank'
+        >
+          Privacy Policy
+        </a>
+      </p>
     </footer>
   );
 };
