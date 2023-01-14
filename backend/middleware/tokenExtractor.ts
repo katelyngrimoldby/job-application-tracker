5b61bf6343270116c3048d1e53d6566c1531cac5
katelyngrimoldby <katelyngrimoldby@gmail.com>
2023-01-13 22:08:03 -0700

    Refactor authorization middleware for refresh token

--- backend/middleware/tokenExtractor.ts
+++ backend/middleware/tokenExtractor.ts
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import { SECRET } from '../util/config';
 import { Response, NextFunction } from 'express';
 import { RequestUserAuth, Signature } from '../types';
+import authService from '../services/authService';
 
 interface RequestUserAuthHandler {
   (req: RequestUserAuth, res: Response, next: NextFunction): void;
@@ -13,6 +14,10 @@ const tokenExtractor: RequestUserAuthHandler = async (req, res, next) => {
     return res.status(401).json({ error: 'Token missing' });
   }
 
+  if (!SECRET) {
+    return res.status(500).json({ error: 'Missing env variable' });
+  }
+
   try {
     req.decodedToken = jwt.verify(
       authorization.substring(7),
@@ -21,7 +26,32 @@ const tokenExtractor: RequestUserAuthHandler = async (req, res, next) => {
 
     next();
   } catch (err) {
-    next(err);
+    if (err instanceof Error) {
+      if (err.name === 'TokenExpiredError') {
+        if (!req.headers['userid']) {
+          return res.status(401).json({ error: 'No user id' });
+        }
+
+        const accessToken = await authService.getSession(
+          Number(req.headers['userid'])
+        );
+
+        if (!accessToken) {
+          return res.status(401).json({ error: 'No refresh token' });
+        }
+
+        try {
+          req.decodedToken = jwt.verify(
+            accessToken,
+            SECRET ? SECRET : 'secret'
+          ) as Signature;
+
+          next();
+        } catch (err) {
+          next(err);
+        }
+      }
+    }
   }
 };
 
