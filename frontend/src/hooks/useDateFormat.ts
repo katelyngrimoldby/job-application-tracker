ed504380afc131b5bb311cdacda991e19c8b8402
katelyngrimoldby <katelyngrimoldby@gmail.com>
2024-12-07 12:57:44 -0700

    split getDate into getShortDate and getLongDate

--- frontend/src/hooks/useDateFormat.ts
+++ frontend/src/hooks/useDateFormat.ts
@@ -1,5 +1,5 @@
-const useDateFormat = (date: Date) => {
-  const getDate = () => {
+const useDateFormat = () => {
+  const getShortDate = (date: Date) => {
     let month = '';
     switch (date.getMonth()) {
       case 0:
@@ -58,7 +58,51 @@ const useDateFormat = (date: Date) => {
     return `${month} ${date.getDate()}${suffix}`;
   };
 
-  const getDateTime = () => {
+  const getLongDate = (date: Date) => {
+    let month = '';
+    switch (date.getMonth()) {
+      case 0:
+        month = 'January';
+        break;
+      case 1:
+        month = 'February';
+        break;
+      case 2:
+        month = 'March';
+        break;
+      case 3:
+        month = 'April';
+        break;
+      case 4:
+        month = 'May';
+        break;
+      case 5:
+        month = 'June';
+        break;
+      case 6:
+        month = 'July';
+        break;
+      case 7:
+        month = 'August';
+        break;
+      case 8:
+        month = 'September';
+        break;
+      case 9:
+        month = 'October';
+        break;
+      case 10:
+        month = 'November';
+        break;
+      case 11:
+        month = 'December';
+        break;
+    }
+
+    return `${month} ${date.getDate()}, ${date.getFullYear()}`;
+  };
+
+  const getDateTime = (date: Date) => {
     let month = '';
     switch (date.getMonth()) {
       case 0:
@@ -102,7 +146,7 @@ const useDateFormat = (date: Date) => {
     return `${month} ${date.getDate()}, ${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}`;
   };
 
-  return { getDate, getDateTime };
+  return { getShortDate, getLongDate, getDateTime };
 };
 
 export default useDateFormat;
