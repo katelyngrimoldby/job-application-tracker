a69acfebc15a2eed151d714ac7776c73fb8109a4
katelyngrimoldby <katelyngrimoldby@gmail.com>
2024-12-08 14:52:13 -0700

    Fix return statements

--- frontend/src/components/Preview/index.tsx
+++ frontend/src/components/Preview/index.tsx
@@ -7,19 +7,23 @@ const Preview = ({ type }: { type: 'applications' | 'interviews' }) => {
   const [{ applications, interviews }] = useStateValue();
 
   if (type === 'applications')
-    <div>
-      <ApplicationCell application={applications[-1]} />
-      <ApplicationCell application={applications[-2]} />
-      <ApplicationCell application={applications[-3]} />
-      <Link to='/applications'>View All</Link>
-    </div>;
+    return (
+      <div>
+        <ApplicationCell application={applications[-1]} />
+        <ApplicationCell application={applications[-2]} />
+        <ApplicationCell application={applications[-3]} />
+        <Link to='/applications'>View All</Link>
+      </div>
+    );
   else
-    <div>
-      <InterviewCell interview={interviews[-1]} />
-      <InterviewCell interview={interviews[-2]} />
-      <InterviewCell interview={interviews[-3]} />
-      <Link to='/interviews'>View All</Link>
-    </div>;
+    return (
+      <div>
+        <InterviewCell interview={interviews[-1]} />
+        <InterviewCell interview={interviews[-2]} />
+        <InterviewCell interview={interviews[-3]} />
+        <Link to='/interviews'>View All</Link>
+      </div>
+    );
 };
 
 export default Preview;
