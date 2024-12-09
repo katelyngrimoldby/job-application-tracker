c5f1122305028f8f8e30d7c28dc02b4e9969d762
katelyngrimoldby <katelyngrimoldby@gmail.com>
2024-12-08 18:37:33 -0700

    Implement submission formatting for addition/updating

--- frontend/src/components/ApplicationForm.tsx
+++ frontend/src/components/ApplicationForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import useInput from '../hooks/useInput';
-import { Application } from '../types';
+import { Application, NewApplication } from '../types';
 import FileUploader from './FileUploader';
 import RichTextEditor from './RichTextEditor';
 import Dropdown from './Dropdown';
@@ -13,21 +13,107 @@ const statusOptions = [
   { label: 'Rejected', value: 'rejected' },
 ];
 
-const ApplicationForm = ({ content }: { content?: Application }) => {
-  const position = useInput('text', content ? content.positionTitle : '');
+const ApplicationForm = ({
+  content,
+  handleAddition,
+  handleUpdate,
+}: {
+  content?: Application;
+  handleAddition?: (submission: NewApplication) => void;
+  handleUpdate?: (submission: NewApplication, id: number) => void;
+}) => {
+  const positionTitle = useInput('text', content ? content.positionTitle : '');
   const company = useInput('text', content ? content.company : '');
   const jobId = useInput('text', content ? content.jobId : '');
   const location = useInput('text', content ? content.location : '');
+  const [status, setStatus] = useState<
+    'applied' | 'assessments' | 'interviewing' | 'rejected' | 'offered'
+  >(content ? content.status : 'applied');
   const [notes, setNotes] = useState(content ? content.notes : '');
+  const [files, setFiles] = useState(content ? content.files : []);
+
+  const getConvertedFiles = (files: string[]) => {
+    setFiles(files);
+  };
+
+  const getStatus = (status: string) => {
+    if (
+      status === 'applied' ||
+      status === 'assessments' ||
+      status === 'interviewing' ||
+      status === 'rejected' ||
+      status === 'offered'
+    ) {
+      setStatus(status);
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const submission: NewApplication = {
+      positionTitle: positionTitle.value,
+      company: company.value,
+      location: location.value,
+      jobId: jobId.value,
+      status,
+      assessmentDate: null,
+      interviewDate: null,
+      offerDate: null,
+      rejectionDate: null,
+      files,
+      notes,
+    };
+
+    if (status == 'assessments' && !content?.assessmentDate)
+      submission.assessmentDate = new Date();
+    else if (status == 'interviewing' && !content?.interviewDate)
+      submission.interviewDate = new Date();
+    else if (status == 'rejected' && !content?.rejectionDate)
+      submission.interviewDate = new Date();
+    else if (status == 'offered' && !content?.offerDate)
+      submission.offerDate = new Date();
+
+    if (content) {
+      if (content.assessmentDate)
+        submission.assessmentDate = content.assessmentDate;
+      else if (status == 'assessments') submission.assessmentDate = new Date();
+      if (content.interviewDate)
+        submission.interviewDate = content.interviewDate;
+      else if (status == 'interviewing') submission.interviewDate = new Date();
+      if (content.rejectionDate)
+        submission.rejectionDate = content.rejectionDate;
+      else if (status == 'rejected') submission.rejectionDate = new Date();
+      if (content.offerDate) submission.offerDate = content.offerDate;
+      else if (status == 'offered') submission.offerDate = new Date();
+
+      if (handleUpdate) handleUpdate(submission, content.id);
+    } else {
+      switch (status) {
+        case 'assessments':
+          submission.assessmentDate = new Date();
+          break;
+        case 'interviewing':
+          submission.interviewDate = new Date();
+          break;
+        case 'offered':
+          submission.offerDate = new Date();
+          break;
+        case 'rejected':
+          submission.rejectionDate = new Date();
+      }
+
+      if (handleAddition) handleAddition(submission);
+    }
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor='position'>Position</label>
         <input
           name='position'
           required
-          {...position}
+          {...positionTitle}
         />
       </div>
       <div>
@@ -66,12 +152,15 @@ const ApplicationForm = ({ content }: { content?: Application }) => {
           values={statusOptions}
           startValue={statusOptions[0]}
           labelledBy='status'
-          handleChange={() => null}
+          handleChange={getStatus}
         />
       </div>
       <div>
         <label htmlFor='files'>Files</label>
-        <FileUploader />
+        <FileUploader
+          handleChange={getConvertedFiles}
+          initFiles={files}
+        />
       </div>
       <div>
         <p
@@ -86,6 +175,7 @@ const ApplicationForm = ({ content }: { content?: Application }) => {
           setContent={setNotes}
         />
       </div>
+      <button type='submit'>Save</button>
     </form>
   );
 };
--- frontend/src/components/InterviewForm.tsx
+++ frontend/src/components/InterviewForm.tsx
@@ -1,25 +1,67 @@
 import { useState } from 'react';
 import useInput from '../hooks/useInput';
+import useDateFormat from '../hooks/useDateFormat';
+import useFind from '../hooks/useFind';
 import { useStateValue } from '../state';
-import { Interview } from '../types';
+import { Interview, NewInterview } from '../types';
 import FileUploader from './FileUploader';
 import RichTextEditor from './RichTextEditor';
 import Dropdown from './Dropdown';
 
-const InterviewForm = ({ content }: { content?: Interview }) => {
+const InterviewForm = ({
+  content,
+  handleUpdate,
+  handleAddition,
+}: {
+  content?: Interview;
+  handleUpdate?: (interview: NewInterview, id: number) => void;
+  handleAddition?: (interview: NewInterview) => void;
+}) => {
+  const { getDateTimeValue } = useDateFormat();
   const [{ applications }] = useStateValue();
+  const { findApplicationForInterview } = useFind();
+  const [applicationId, setApplicationId] = useState(
+    content ? content.applicationId : applications[0].id
+  );
+  const application = findApplicationForInterview(applicationId);
   const contact = useInput('text', content ? content.contact : '');
   const website = useInput('text', content ? content.website : '');
   const dateTime = useInput(
     'datetime-local',
-    content
-      ? `${content.time.getFullYear()}-${content.time.getMonth() + 1}-${content.time.getDate()}T${content.time.getHours()}:${content.time.getMinutes()}`
-      : ''
+    content ? getDateTimeValue(content.time) : ''
   );
   const [notes, setNotes] = useState(content ? content.notes : '');
+  const [files, setFiles] = useState(content ? content.files : []);
+
+  const getConvertedFiles = (files: string[]) => {
+    setFiles(files);
+  };
+
+  const getApplicationId = (id: string) => {
+    setApplicationId(Number(id));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const submission: NewInterview = {
+      files,
+      notes,
+      applicationId,
+      time: new Date(dateTime.value),
+    };
+
+    if (contact.value) submission.contact = contact.value;
+    if (website.value) submission.website = website.value;
+
+    if (content) {
+      if (handleUpdate) handleUpdate(submission, content.id);
+    } else {
+      if (handleAddition) handleAddition(submission);
+    }
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <p
           id='application'
@@ -35,7 +77,11 @@ const InterviewForm = ({ content }: { content?: Interview }) => {
               value: String(application.id),
             };
           })}
-          handleChange={() => null}
+          startValue={{
+            label: `${application.positionTitle} @ ${application.company}`,
+            value: String(application.id),
+          }}
+          handleChange={getApplicationId}
           labelledBy='application'
         />
       </div>
@@ -63,7 +109,10 @@ const InterviewForm = ({ content }: { content?: Interview }) => {
         </div>
         <div>
           <label htmlFor='files'>Files</label>
-          <FileUploader />
+          <FileUploader
+            handleChange={getConvertedFiles}
+            initFiles={files}
+          />
         </div>
       </div>
       <div>
@@ -79,6 +128,7 @@ const InterviewForm = ({ content }: { content?: Interview }) => {
           setContent={setNotes}
         />
       </div>
+      <button type='submit'>Save</button>
     </form>
   );
 };
