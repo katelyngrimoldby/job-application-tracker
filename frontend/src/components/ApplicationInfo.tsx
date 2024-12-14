02b75335f57912d7671f767a5b3018187b95e276
katelyngrimoldby <katelyngrimoldby@gmail.com>
2024-12-14 15:03:54 -0700

    Styling

--- frontend/src/components/ApplicationInfo.tsx
+++ frontend/src/components/ApplicationInfo.tsx
@@ -6,6 +6,7 @@ import { Application } from '../types';
 import ReadOnlyRichText from './RichTextEditor/ReadOnly';
 import PencilIcon from './icons/PencilIcon';
 import TrashIcon from './icons/TrashIcon';
+import styles from '../styles/components/ApplicationInfo.module.css';
 
 const ApplicationInfo = ({
   application,
@@ -24,17 +25,21 @@ const ApplicationInfo = ({
   return (
     <>
       <section>
-        <h1>{application.positionTitle}</h1>
-        <div>
-          <span>{application.company}</span>
-          <span>{application.status}</span>
-        </div>
-        <div>
+        <header>
+          <h1 className={styles.position}>{application.positionTitle}</h1>
+          <span className={styles.company}>{application.company}</span>
+        </header>
+        <div className={styles.secondary}>
+          <span className={styles.status}>
+            Status:{' '}
+            {application.status.charAt(0).toUpperCase() +
+              application.status.substring(1)}
+          </span>
           <span>Job ID: {application.jobId}</span>
           <span>Location: {application.location}</span>
         </div>
       </section>
-      <div>
+      <div className={styles.split}>
         <section>
           <h2>Timeline</h2>
           <ul>
@@ -60,37 +65,55 @@ const ApplicationInfo = ({
         <section>
           <h2>Interviews</h2>
           <ul>
-            {interviews.map((interview) => (
-              <li>
-                <Link to={`/interviews/${interview.id}`}>
-                  {getDateTime(interview.time)} with {interview.contact}
-                </Link>
-              </li>
-            ))}
+            {interviews.length > 0 ? (
+              interviews.map((interview) => (
+                <li>
+                  <Link
+                    to={`/interviews/${interview.id}`}
+                    key={interview.id}
+                  >
+                    {getDateTime(interview.time)} with {interview.contact}
+                  </Link>
+                </li>
+              ))
+            ) : (
+              <Link
+                to='/interviews/new'
+                className={styles.new}
+              >
+                Add interview
+              </Link>
+            )}
           </ul>
         </section>
       </div>
       <section>
         <h2>Files and Notes</h2>
-        <ul>
-          {files.map((file) => (
-            <li>
-              <button
-                onClick={() => {
-                  const pdfWindow = window.open('');
-                  pdfWindow?.document.write(
-                    `<iframe width="100%" height="100%" src="${URL.createObjectURL(file as Blob)}"></iframe>`
-                  );
-                }}
-              >
-                {file.name}
-              </button>
-            </li>
-          ))}
-        </ul>
-        <ReadOnlyRichText content={application.notes} />
+        <div className={styles.assets}>
+          <ul>
+            {files.length > 0 ? (
+              files.map((file) => (
+                <li key={file.name}>
+                  <button
+                    onClick={() => {
+                      const pdfWindow = window.open('');
+                      pdfWindow?.document.write(
+                        `<iframe width="100%" height="100%" src="${URL.createObjectURL(file as Blob)}"></iframe>`
+                      );
+                    }}
+                  >
+                    {file.name}
+                  </button>
+                </li>
+              ))
+            ) : (
+              <p>No files</p>
+            )}
+          </ul>
+          <ReadOnlyRichText content={application.notes} />
+        </div>
       </section>
-      <div>
+      <div className={styles.buttons}>
         <Link to={`/applications/${application.id}/edit`}>
           <PencilIcon />
         </Link>
