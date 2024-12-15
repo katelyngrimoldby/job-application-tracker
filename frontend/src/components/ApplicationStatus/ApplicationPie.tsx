1745b09eb7ee56553dbb397c4c1cd64f1907c6b2
katelyngrimoldby <katelyngrimoldby@gmail.com>
2024-12-15 14:30:50 -0700

    Add styling to chart

--- frontend/src/components/ApplicationStatus/ApplicationPie.tsx
+++ frontend/src/components/ApplicationStatus/ApplicationPie.tsx
@@ -1,20 +1,34 @@
 import { Pie } from 'react-chartjs-2';
 import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
+import { useStateValue } from '../../state';
 
 Chart.register(ArcElement, Tooltip, Legend);
 
 const ApplicationPie = ({ data }: { data: Number[] }) => {
+  const [{ theme }] = useStateValue();
+
   const chartData = {
     labels: ['Applied', 'Assessments', 'Interviews', 'Offers'],
     datasets: [
       {
         data,
         borderWidth: 2,
+        backgroundColor: ['#005bd1', '#00a0d1', '#00d1b9', '#00d134'],
+        borderColor: theme == 'dark' ? '#242831' : '#f2f6fc',
       },
     ],
   };
 
-  return <Pie data={chartData} />;
+  const legendOptions = {
+    color: theme == 'dark' ? '#f2f6fc' : '#242831',
+  };
+
+  return (
+    <Pie
+      data={chartData}
+      options={{ plugins: { legend: { labels: legendOptions } } }}
+    />
+  );
 };
 
 export default ApplicationPie;
