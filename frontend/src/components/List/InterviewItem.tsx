d25831e30067efc57193ed0ffa6d9f1d36566298
katelyngrimoldby <katelyngrimoldby@gmail.com>
2024-12-15 10:47:20 -0700

    Add stylesheet

--- frontend/src/components/List/InterviewItem.tsx
+++ frontend/src/components/List/InterviewItem.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import useDateFormat from '../../hooks/useDateFormat';
 import { Interview, Application } from '../../types';
 import ArrowIcon from '../icons/ArrowIcon';
+import styles from '../../styles/components/ListItem.module.css';
 
 const ListInterviewItem = ({
   interview,
@@ -12,24 +13,25 @@ const ListInterviewItem = ({
   application: Application;
 }) => {
   const [visible, setVisible] = useState(false);
-  const { getShortDate } = useDateFormat();
+  const { getShortDate, getTime } = useDateFormat();
 
   return (
-    <li>
-      <div>
+    <li className={styles.wrapper}>
+      <div className={styles.primaryInfo}>
         <span>{application.positionTitle}</span>
         <span>{application.company}</span>
         <span>{getShortDate(interview.time)}</span>
-        <button onClick={() => setVisible(!visible)}>
+        <button
+          onClick={() => setVisible(!visible)}
+          className={visible ? styles.collapseBtn : styles.expandBtn}
+        >
           <ArrowIcon />
         </button>
       </div>
-      <div>
+      <div className={visible ? styles.extraVisible : styles.extraInfo}>
         <span>{interview.contact}</span>
-        <span>
-          {interview.time.getHours()}:{interview.time.getMinutes()}
-        </span>
-        <Link to={`/interviews/${interview.id}`}></Link>
+        <span>{getTime(interview.time)}</span>
+        <Link to={`/interviews/${interview.id}`}>View Interview</Link>
       </div>
     </li>
   );
