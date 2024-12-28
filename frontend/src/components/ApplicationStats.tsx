e99e8cc39fba0fcf8b45e203793bdd5c95414d1c
katelyngrimoldby <katelyngrimoldby@gmail.com>
2024-12-28 15:21:11 -0700

    Add test ids

--- frontend/src/components/ApplicationStats.tsx
+++ frontend/src/components/ApplicationStats.tsx
@@ -3,10 +3,10 @@ import styles from '../styles/components/ApplicationStats.module.css';
 const ApplicationStats = ({ data }: { data: Number[] }) => {
   return (
     <div className={styles.wrapper}>
-      <span>{`${String(data[0])} ${data[0] == 1 ? 'Application' : 'Applications'}`}</span>
-      <span>{`${String(data[1])} ${data[1] == 1 ? 'Assessment' : 'Assessments'}`}</span>
-      <span>{`${String(data[2])} ${data[2] == 1 ? 'Interview' : 'Interviews'}`}</span>
-      <span>{`${String(data[3])} ${data[3] == 1 ? 'Offer' : 'Offers'}`}</span>
+      <span data-testid='applications'>{`${String(data[0])} ${data[0] == 1 ? 'Application' : 'Applications'}`}</span>
+      <span data-testid='assessments'>{`${String(data[1])} ${data[1] == 1 ? 'Assessment' : 'Assessments'}`}</span>
+      <span data-testid='interviews'>{`${String(data[2])} ${data[2] == 1 ? 'Interview' : 'Interviews'}`}</span>
+      <span data-testid='offers'>{`${String(data[3])} ${data[3] == 1 ? 'Offer' : 'Offers'}`}</span>
     </div>
   );
 };
