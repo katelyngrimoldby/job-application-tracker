091f7937cdc6c45ebcc71b687ea5b0fc75f0c7b0
katelyngrimoldby <katelyngrimoldby@gmail.com>
2024-12-28 15:25:12 -0700

    Adjust config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  viewportWidth: 1024,
+  watchForFileChanges: false,
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
