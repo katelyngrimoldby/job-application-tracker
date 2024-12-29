1ce778bd54c50fa3279db9f4d712f1177d3b1c5c
katelyngrimoldby <katelyngrimoldby@gmail.com>
2024-12-28 21:43:50 -0700

    Revert config changes

--- backend/util/config.ts
+++ backend/util/config.ts
@@ -1,23 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
-let POSTGRES_USER: string | undefined;
-let POSTGRES_PASS: string | undefined;
-let POSTGRES_DB: string | undefined;
+const POSTGRES_DB = process.env.POSTGRES_DB;
 
-if (process.env.POSTGRES_URL) {
-  const url = process.env.POSTGRES_URL.replace('postgresql://', '');
+const POSTGRES_USER = process.env.POSTGRES_USER;
 
-  POSTGRES_USER = url.substring(0, url.indexOf(':'));
-  POSTGRES_PASS = url.substring(url.indexOf(':') + 1, url.indexOf('@'));
-  POSTGRES_DB = url.substring(url.indexOf('/'));
-} else {
-  POSTGRES_DB = process.env.POSTGRES_DB;
-
-  POSTGRES_USER = process.env.POSTGRES_USER;
-
-  POSTGRES_PASS = process.env.POSTGRES_PASS;
-}
+const POSTGRES_PASS = process.env.POSTGRES_PASS;
 
 const REDIS_URL = process.env.REDIS_URL;
 
